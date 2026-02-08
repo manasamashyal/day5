da3a452e969cfefc75224b9c9a35379148fce308
manasamashyal <110389157+manasamashyal@users.noreply.github.com>
2026-02-08 23:32:17 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    fact = 1
+    for i in range (1, n + 1):
+        fact = fact * i
+    return fact
+num = int(input("Enter a number: "))
+if num < 0:
+   print("Factorial is not defined for negative numbers")
+else:
+    print("Factorial of",num, "is", factorial(num) )
+
+
+
+
+
